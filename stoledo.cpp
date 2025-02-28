3dd2cb7854b9b3181fd3dc0c604f77d363526019
Sean Toledo <stoledo@odin.cs.csubak.edu>
2025-02-28 15:33:45 -0800

    sean added

--- stoledo.cpp
+++ stoledo.cpp
@@ -1,3 +1,17 @@
 //Sean Toledo
-//Group Project
+//Date : Spring 2025
 //Blok
+//
+#include<iostream>
+#include"fonts.h"
+#include"function.h"
+using namespace std;
+
+void seanEndCredit (void)
+{
+    Rect title;
+    title.bot = 18;
+    title.left = 10;
+    title.center = 0;
+    ggprint8b(&title, 16, 0x00ff0000, "Author 5: Sean Toledo");
+}
